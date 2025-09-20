aea6536dd5e60792471c10ff2c7d836f0fdd1627
poligon98 <tj.chud@gmail.com>
2025-09-20 18:52:06 +0700

    Create main.py
    put code in here

---
+++ PacMan/main.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+
+# Set up background
+# Set up walls
+
+
+# Set up sprites
+# Pac Man
+# Ghost
+# Dots
+
+# Processing Functions
+
+# Game Loop
+while True:
+    # Input
+    # Other process
+    # Display scene
+    # Game over condition
+    break
